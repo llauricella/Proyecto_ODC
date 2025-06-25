d2db50881421ae0f21031a6e626d51046e7daa86
leHeix <gordosatiro@protonmail.com>
2025-06-24 20:31:00 -0400

    Función para vaciar tablero en pantalla

--- battleship.asm
+++ battleship.asm
@@ -13,6 +13,12 @@
 	# ADDRESS: 0x10010000
 	screen_board: .space 512 # El tablero se aloja al inicio de la sección data, reservar espacio de tablero
 	
+	# Paleta de colores
+	screen_colors:
+		.word 0x0000FF # Azul
+		.word 0x00FF00 # Verde
+		.word 0xFF0000 # Rojo
+		 
 	# Mensajes del menú
 	menu_start_msg: .asciiz "----- BATTLESHIP -----\n\nIntroduce una de las siguientes opciones para continuar:\n\n1 - Jugar contra el CPU\n2 - Jugar contra otro jugador\n3 - Salir\n-> "
 	menu_arrow: .asciiz "-> "
@@ -21,14 +27,13 @@
 	.globl main
 	
 main:
+	jal empty_screen_board
+	
 	show_menu:
 		print_message(menu_start_msg)
 		
 	process_menu_input:
 		read_integer
-		
-		li $t0, 1 # Lower bound
-		li $t1, 3 # Upper bound
 		
 		# Validar input
 		blt $v0, 1, invalid_menu_input
@@ -48,6 +53,22 @@ main:
 	menu_option_2:
 	menu_option_3:
 		b exit
+
+# Función para vaciar el tablero en pantalla
+empty_screen_board:
+	li $t1, 0
+	get_from_array($t0, screen_colors, 0)
+	
+	loop_esb:
+		sw $t0, screen_board($t1)
+		
+		beq $t1, 512, return_esb
+		
+		add $t1, $t1, 4
+		j loop_esb
+		
+	return_esb:
+		jr $ra
 		
 exit:
 	li $v0, 10
--- macros.asm
+++ macros.asm
@@ -15,4 +15,24 @@
 	la $a0, %msg
 	syscall
 .end_macro
+
+# Almacena %addr(%idx * 4) en %reg
+# Registros $t8 y $t9 reservados
+.macro get_from_array(%reg, %addr, %idx)
+	# Pushea $t8, $t9 al stack para preservar sus valores
+	sub $sp, $sp, 8
+	sw $t8, 4($sp)
+	sw $t9, 0($sp)
 	
+	li $t8, %idx
+	mul $t9, $t8, 4
+	
+	la $t8, %addr
+	add $t8, $t8, $t9
+	lw %reg, ($t8)
+	
+	# Restaura los registros $t8, $t9 del stack
+	lw $t9, 0($sp)
+	lw $t8, 4($sp)
+	add $sp, $sp, 8
+.end_macro
